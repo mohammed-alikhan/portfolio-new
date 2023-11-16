83c29cadfe4acc2b7c7c93c339d6761a642ea15a
Mohammed Ali Khan <73744784+mohammed-alikhan@users.noreply.github.com>
2023-11-16 01:43:32 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -244,54 +244,6 @@ function bodyScrollingToggle() {
 })();
 
 
-/* ------------------------------------testimonial slider--------------------------------
-
-   (()=>{
-
-      const sliderContainer = document.querySelector(".testi-slider-container"),
-      slides = sliderContainer.querySelectorAll(".testi-item"),
-      slideWidth = sliderContainer.offsetWidth;
-      prevBtn = document.querySelector(".testi-slider-nav .prev"),
-      nextBtn = document.querySelector(".testi-slider-nav .next"),
-      activeSlide = sliderContainer.querySelector(".testi-item.active");
-      let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
-
-      //setting width of all slides
-      slides.forEach((slide) =>{
-         slide.style.width = slideWidth + "px";
-      })
-
-      //setting width of sliderContainer
-      sliderContainer.style.width = slideWidth * slides.length + "px";
-
-      nextBtn.addEventListener("click", () =>{
-         if(slideIndex === slides.length -1){
-            slideIndex = 0;
-         }else{
-            slideIndex++;
-         }
-         slider();
-      })
-
-      prevBtn.addEventListener("click", () =>{
-         if(slideIndex === 0){
-            slideIndex = slides.length -1;
-         }else{
-            slideIndex--;
-         }
-         slider();
-      })
-
-      function slider(){
-         // deactivating existing active slides 
-         sliderContainer.querySelector(".testi-item.active").classList.remove("active");
-         // activating new slides
-         slides[slideIndex].classList.add("active");
-         sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + "px";
-      }
-      slider();
-
-   })(); */
 
    window.addEventListener("load", () => {
       const preloader = document.querySelector(".preloader");
